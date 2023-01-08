92b036e6c2726be7342c1e0f367f437e9aae42d2
cindyhont <cindyhont@gmail.com>
2023-01-08 19:53:56 +0000

    .

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/cindyhont/projmgmt-server-pubsub/pubsub"
+)
 
 func init() {
 	fmt.Println("server pubsub init complete")
 }
 
 func main() {
-
+	pubsub.Listen()
 }
